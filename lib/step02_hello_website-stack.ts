fdbec4f8606dd8903292b4c50a60717ad7d1efba
Shiraz Javed Farooq <114878673+EsJayEff@users.noreply.github.com>
2023-09-05 23:07:28 +0500

    Completed

--- lib/step02_hello_website-stack.ts
+++ lib/step02_hello_website-stack.ts
@@ -1,6 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
 
 export class Step02HelloWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +18,29 @@ export class Step02HelloWebsiteStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'Step02HelloWebsiteQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+      // create a bucket to upload your app files
+
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      versioned: true,
+    });
+
+    const distribution = new cloudfront.Distribution(this, "bootcampDistribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+      },
+      defaultRootObject: "index.html",
+    });
+
+    new cdk.CfnOutput(this, "DistributionDomainName", {
+      value: distribution.domainName,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./website")],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
   }
 }
